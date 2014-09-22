45b76a1c2fc4b3a4dad93d7841c37d249bd73b26
Brian Lai <brian.lai.ym@gmail.com>
2014-09-22 14:08:07 -0700

    clean up test file format

--- test/base.js
+++ test/base.js
@@ -29,42 +29,42 @@ function filesToVinylStream(files) {
 }
 
 function expectedFile(file) {
-	var f = fs.readFileSync(path.resolve(__dirname, 'expects', file));
-	return _.template(f, {
-		base: base
-	});
+  var f = fs.readFileSync(path.resolve(__dirname, 'expects', file));
+  return _.template(f, {
+    base: base
+  });
 }
 
 describe('Empty cases', function() {
-  it('should produce index.scss vinyl with just comments', function (done) {
-  	var stream = inuit();
+  it('should produce index.scss vinyl with just comments', function(done) {
+    var stream = inuit();
     var expected = expectedFile('1.scss');
 
-  	stream.on('error', function(err) {
-  	  should.exist(err);
-  	  console.log(err);
-  	  done(err);
-  	});
+    stream.on('error', function(err) {
+      should.exist(err);
+      console.log(err);
+      done(err);
+    });
 
-  	stream.on('data', function(newFile) {
-  	  should.exist(newFile);
-  	  should.exist(newFile.contents);
+    stream.on('data', function(newFile) {
+      should.exist(newFile);
+      should.exist(newFile.contents);
 
-  	  (newFile.contents + '\n').should.equal(expected);
-  	  done();
-  	});
+      (newFile.contents + '\n').should.equal(expected);
+      done();
+    });
   });
 });
 
 describe('Module sorting', function() {
-	var files = [
-	  'bower_components/inuit-box-sizing/_generic.box-sizing.scss',
-	  'bower_components/inuit-defaults/_settings.defaults.scss',
-	  'bower_components/inuit-functions/_tools.functions.scss',
-	  'bower_components/inuit-mixins/_tools.mixins.scss',
-	  'bower_components/inuit-normalize/_generic.normalize.scss',
-	  'bower_components/inuit-page/_base.page.scss'
-	];
+  var files = [
+    'bower_components/inuit-box-sizing/_generic.box-sizing.scss',
+    'bower_components/inuit-defaults/_settings.defaults.scss',
+    'bower_components/inuit-functions/_tools.functions.scss',
+    'bower_components/inuit-mixins/_tools.mixins.scss',
+    'bower_components/inuit-normalize/_generic.normalize.scss',
+    'bower_components/inuit-page/_base.page.scss'
+  ];
 
   it('should sort default inuit sections in order', function(done) {
     var expected = expectedFile('2.scss');
@@ -87,8 +87,8 @@ describe('Module sorting', function() {
     });
   });
 
-  it('should add any unrecognized section to the end', function (done) {
-  	var filesWithLocal = files.concat('local/other.scss');
+  it('should add any unrecognized section to the end', function(done) {
+    var filesWithLocal = files.concat('local/other.scss');
     var expected = expectedFile('3.scss');
 
     var fileStream = filesToVinylStream(filesWithLocal);
@@ -110,12 +110,12 @@ describe('Module sorting', function() {
   });
 });
 
-describe('Options', function () {
-	it('should be able to customize `name` for the output vinyl object', function (done) {
-		var name = 'main';
-		var stream = inuit(filesToVinylStream([]), {
-			name: name
-		});
+describe('Options', function() {
+  it('should be able to customize `name` for the output vinyl object', function(done) {
+    var name = 'main';
+    var stream = inuit(filesToVinylStream([]), {
+      name: name
+    });
 
     stream.on('error', function(err) {
       should.exist(err);
@@ -127,5 +127,5 @@ describe('Options', function () {
       newFile.path.should.equal(path.resolve(name + '.scss'));
       done();
     });
-	});
+  });
 });
